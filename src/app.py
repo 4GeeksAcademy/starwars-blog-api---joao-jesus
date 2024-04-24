c6ebc963a0aeac2847fb2c4736f94a0010144cc8
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-04-24 18:55:41 +0000

    final commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,25 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    people = People.query.all()
+    all_people = list(map(lambda x: x.serialize(), people))
+    
+    return jsonify(all_people), 200
+
+
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+
+    planet = Planets.query.all()
+    all_planets = list(map(lambda x: x.serialize(), planet))
+    return jsonify(all_planets), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,49 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    age = db.Column(db.Integer)
+    heigth = db.Column(db.Integer)
+    gender = db.Column(db.String(250))
+    eyecolor = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "heigth": self.heigth,
+            "gender": self.gender,
+            "eyecolor": self.eyecolor,
+            
+            # do not serialize the password, its a security breach
+        }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer)
+    gravity = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+   
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "population": self.population,
+            
+            
+            # do not serialize the password, its a security breach
+        }
