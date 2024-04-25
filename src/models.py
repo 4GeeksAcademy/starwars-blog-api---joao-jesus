f1a86a4c7bcc4471f78a2da71c3fc2266cd2071c
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-04-25 15:37:42 +0000

    relations

--- src/models.py
+++ src/models.py
@@ -5,26 +5,28 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
+    people = db.relationship('People', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
 
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     age = db.Column(db.Integer)
-    heigth = db.Column(db.Integer)
+    height = db.Column(db.Integer)
     gender = db.Column(db.String(250))
     eyecolor = db.Column(db.String(250))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
 
     def __repr__(self):
         return '<People %r>' % self.name
@@ -34,20 +36,18 @@ class People(db.Model):
             "id": self.id,
             "name": self.name,
             "age": self.age,
-            "heigth": self.heigth,
+            "height": self.height,
             "gender": self.gender,
             "eyecolor": self.eyecolor,
-            
-            # do not serialize the password, its a security breach
         }
-    
+
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
-    diameter = db.Column(db.Integer)
-    gravity = db.Column(db.Integer)
+    diameter = db.Column(db.Float)
+    gravity = db.Column(db.Float)
     population = db.Column(db.Integer)
-   
+    people = db.relationship('People', backref='planet', lazy=True)
 
     def __repr__(self):
         return '<Planets %r>' % self.name
@@ -59,7 +59,4 @@ class Planets(db.Model):
             "diameter": self.diameter,
             "gravity": self.gravity,
             "population": self.population,
-            
-            
-            # do not serialize the password, its a security breach
         }
